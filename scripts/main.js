b7faf3769abe619dfee899dfd6ab92dfb441dda2
JamesKlaesges <50339142+JamesKlaesges@users.noreply.github.com>
2020-03-10 20:03:16 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -32,24 +32,25 @@ function CalculateTransformedVertex(vertex) {
 
 // automatically called whenever user modifies a transform (changes type or values)
 function ChangeTransform(index, type, values) {
+    var mat4x4 = app.transforms[index].mat4x4;
     app.transforms[index].type = type;
     if (type == "translate"){
-        app.transforms[index].mat4x4 = Mat4x4Translate(app.transforms[index].mat4x4, values[0], values[1], values[2]);
+        app.transforms[index].mat4x4 = Mat4x4Translate(mat4x4, values[0], values[1], values[2]);
     }
     else if (type == "scale"){
-        app.transforms[index].mat4x4 = Mat4x4Scale(app.transforms[index].mat4x4, values[0], values[1], values[2]);
+        app.transforms[index].mat4x4 = Mat4x4Scale(mat4x4, values[0], values[1], values[2]);
     }
     else if (type == "rotate_x"){
-        app.transforms[index].mat4x4 = Mat4x4RotateX(app.transforms[index].mat4x4, values[0]);
+        app.transforms[index].mat4x4 = Mat4x4RotateX(mat4x4, values[0]);
     }
     else if (type == "rotate_y"){
-        app.transforms[index].mat4x4 = Mat4x4RotateY(app.transforms[index].mat4x4, values[0]);
+        app.transforms[index].mat4x4 = Mat4x4RotateY(mat4x4, values[0]);
     }
     else if (type == "rotate_z"){
-        app.transforms[index].mat4x4 = Mat4x4RotateZ(app.transforms[index].mat4x4, values[0]);
+        app.transforms[index].mat4x4 = Mat4x4RotateZ(mat4x4, values[0]);
     }
     else if (type == "shear"){
-        app.transforms[index].mat4x4 = Mat4x4ShearXY(app.transforms[index].mat4x4, values[0], values[1]);
+        app.transforms[index].mat4x4 = Mat4x4ShearXY(mat4x4, values[0], values[1]);
     }
 
     // recalculate compound transform and tranformed vertex
