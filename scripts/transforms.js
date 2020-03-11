910e51eec96094eccdee8250d8b26cb7f8c46d3d
JamesKlaesges <50339142+JamesKlaesges@users.noreply.github.com>
2020-03-10 20:19:27 -0700

    Update transforms.js

--- scripts/transforms.js
+++ scripts/transforms.js
@@ -5,38 +5,38 @@ function Mat4x4Identity(mat4x4) {
 
 // set values of mat4x4 to the translate matrix
 function Mat4x4Translate(mat4x4, tx, ty, tz) {
-    mat4x4 = new Matrix(4, 4);
-    mat4x4.set values([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [tx, ty, tz, 1]]);
+    var mat4x4 = new Matrix(4, 4);
+    mat4x4.values = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [tx, ty, tz, 1]];
 }
 
 // set values of mat4x4 to the scale matrix
 function Mat4x4Scale(mat4x4, sx, sy, sz) {
-    mat4x4 = new Matrix(4, 4);
-    mat4x4.set values([[sx, 0, 0, 0], [0, sy, 0, 0], [0, 0, sz, 0], [0, 0, 0, 1]]);
+    var mat4x4 = new Matrix(4, 4);
+    mat4x4.values = [[sx, 0, 0, 0], [0, sy, 0, 0], [0, 0, sz, 0], [0, 0, 0, 1]];
 }
 
 // set values of mat4x4 to the rotate about x-axis matrix
 function Mat4x4RotateX(mat4x4, theta) {
     mat4x4 = new Matrix(4, 4);
-    mat4x4.set values([[1, 0, 0, 0], [0, Math.cos(theta), Math.sin(theta), 0], [0, -(Math.sin(theta)), -(Math.cos(theta)), 0], [0, 0, 0, 1]]);
+    mat4x4.values = [[1, 0, 0, 0], [0, Math.cos(theta), Math.sin(theta), 0], [0, -(Math.sin(theta)), -(Math.cos(theta)), 0], [0, 0, 0, 1]];
 }
 
 // set values of mat4x4 to the rotate about y-axis matrix
 function Mat4x4RotateY(mat4x4, theta) {
-     mat4x4 = new Matrix(4, 4);
-     mat4x4.set values([[Math.cos(theta), 0, -(Math.sin(theta)), 0], [0, 1, 0, 0], [Math.sin(theta), 0, Math.cos(theta), 0], [0, 0, 0, 1]]);
+     var mat4x4 = new Matrix(4, 4);
+     mat4x4.values = [[Math.cos(theta), 0, -(Math.sin(theta)), 0], [0, 1, 0, 0], [Math.sin(theta), 0, Math.cos(theta), 0], [0, 0, 0, 1]];
 }
 
 // set values of mat4x4 to the rotate about z-axis matrix
 function Mat4x4RotateZ(mat4x4, theta) {
-     mat4x4 = new Matrix(4, 4);
-     mat4x4.set values([[Math.cos(theta), Math.sin(theta), 0, 0], [-(Math.sin(theta)), Math.cos(theta), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]);
+     var mat4x4 = new Matrix(4, 4);
+     mat4x4.values = [[Math.cos(theta), Math.sin(theta), 0, 0], [-(Math.sin(theta)), Math.cos(theta), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
 }
 
 // set values of mat4x4 to the shear parallel to the xy-plane matrix
 function Mat4x4ShearXY(mat4x4, shx, shy) {
-    mat4x4 = new Matrix(4, 4);
-    mat4x4.set values([[1, 0, 0, 0], [0, 1, 0, 0], [shx, shy, 1, 0], [0, 0, 0, 1]]);
+    var mat4x4 = new Matrix(4, 4);
+    mat4x4.values = [[1, 0, 0, 0], [0, 1, 0, 0], [shx, shy, 1, 0], [0, 0, 0, 1]];
 }
 
 // set the x,y,z values of a 3-component vector
